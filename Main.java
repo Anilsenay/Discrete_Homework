b500a436351f47772eaa2450d42042f1b906345f
anilsenay <anilsenay3@gmail.com>
2019-12-07 17:33:51 +0300

    finished

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,6 +20,8 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    
+    public static boolean output[];
     public static Boolean array[][];
     public static void main(String[] args) {
         File file = new File("src/main/java/input.cnf"); 
@@ -42,18 +46,46 @@ public class Main {
         }catch(Exception e){
             System.out.println(e);
         }
-
-        boolean testArray[] = {true,false,false};
-        for(int i = 0; i < testArray.length; i++){
-            System.out.print(testArray[i]+", ");
+        
+        boolean test[] = new boolean[array[0].length];
+        
+        for(int i = 0; i < test.length; i++){
+            if(permute(test, 0))
+                break;
+            test[i] = true;
         }
-        System.out.println();
+        if(output != null){
+            String outputStr = "";
+            for(int i = 0; i < output.length; i++){
+                if(output[i])
+                    outputStr += (i+1) + " ";
+                else
+                    outputStr += (-(i+1)) + " ";
+            }
+            System.out.println("Writing to output file: " + outputStr);
+            try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.cnf"));
+            writer.write("SAT\n\n");
+            writer.write(outputStr);
+     
+            writer.close();
+            }catch(Exception e){ System.out.println(e); };
+        }else{
+                        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.cnf"));
+            writer.write("UNSAT");     
+            writer.close();
+            }catch(Exception e){ System.out.println(e); };
+        }
+    }
+    
+    static boolean testExpression(boolean[] testArray){
+
         boolean expression = true;
         
         for(int i = 0; i < array.length; i++){
             boolean tempExpression = false;
             for(int j = 0; j < array[0].length; j++){
-                System.out.println(i+","+j+"->"+array[i][j]);
                 if(array[i][j] == null)
                     continue;
                 else if(array[i][j] == true){
@@ -64,13 +96,32 @@ public class Main {
                 }
 
             }
-            System.out.println("--->" + tempExpression);
             expression = expression && tempExpression;
         }
-        
-        System.out.println(expression);
-        
-
+        if(expression == true){
+            System.out.println("Expression:" + expression);
+            output = new boolean[array[0].length];
+            for(int i = 0; i < testArray.length; i++){
+                System.out.println(testArray[i]);
+                output[i] = testArray[i];
+            }
+        }
+        return expression;
+    }
+    
+    public static boolean permute(boolean[] intArray, int start) {
+        for(int i = start; i < intArray.length; i++){
+            boolean temp = intArray[start];
+            intArray[start] = intArray[i];
+            intArray[i] = temp;
+            permute(intArray, start + 1);
+            intArray[i] = intArray[start];
+            intArray[start] = temp;
+        }
+        if (start == intArray.length - 1) {
+            return testExpression(intArray);
+        }
+        return false;
     }
     
 }
